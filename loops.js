14356c00af93b8812f3be1261fd77b6581071a5b
IBIRASA <d.ishimwe@alustudent.com>
2023-11-23 10:15:14 +0200

    conditions

---
+++ loops.js
@@ -0,0 +1,14 @@
+let Divine, Titi, Linda, Vanessa;
+Divine = 20;
+Titi = 25;
+Linda = 30;
+Vanessa = 40;
+
+if(Titi > Divine)
+{
+    console.log("Titi is " +Titi+ " years old and Divine is " +Divine+ "years old. This means Titi is the oldest");
+}
+else if (Linda < Vanessa)
+{
+    console.log("Linda is" +Linda+ " years old and Vanessa is " +Vanessa+ "years old. This means Linda is the youngest");
+}
