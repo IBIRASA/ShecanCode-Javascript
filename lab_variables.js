2c799286895ba7bf4c9ee46aa0262905c6d8a0e9
IBIRASA <d.ishimwe@alustudent.com>
2023-11-28 11:17:38 +0200

    updated

--- lab_variables.js
+++ lab_variables.js
@@ -1,11 +1,11 @@
 let name_1 = "Maxwell Wright";
-let phone_1 = 01917196495;
+let phone_1 = "0(1917)196495";
 let email_1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
 let name_2 = "Raja Villarreal";
-let phone_2 = 08663982895;
+let phone_2 = "0866 398 2895";
 let email_2 = "posuere.vulputate@sed.com";
 let name_3 = "Helen Richards";
-let phone_3 = 08001111;
+let phone_3 = "0800 1111";
 let email_3 = "	libero@convallis.edu";
 console.log("Credentials of the first record: \n ");
 console.log("Names:" + name_1+ "\nTelephone number: " + phone_1 + "\nEmail:" + email_1 );
