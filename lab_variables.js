8e6bd0eb03fb89be3c98794fbfcc93f939468a68
IBIRASA <d.ishimwe@alustudent.com>
2023-11-28 11:12:09 +0200

    lab_variables

---
+++ lab_variables.js
@@ -0,0 +1,14 @@
+let name_1 = "Maxwell Wright";
+let phone_1 = 01917196495;
+let email_1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let name_2 = "Raja Villarreal";
+let phone_2 = 08663982895;
+let email_2 = "posuere.vulputate@sed.com";
+let name_3 = "Helen Richards";
+let phone_3 = 08001111;
+let email_3 = "	libero@convallis.edu";
+console.log("Credentials of the first record: \n ");
+console.log("Names:" + name_1+ "\nTelephone number: " + phone_1 + "\nEmail:" + email_1 );
+console.log("\n")
+console.log("Credentials of the last record: \n ");
+console.log("Names:" + name_3+ "\nTelephone number: " + phone_3 + "\nEmail:" + email_3 );
